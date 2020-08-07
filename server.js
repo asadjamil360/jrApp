d2d52dbc320ac2257eeba770a20bf1e34566e6b8
unknown <asadjamil360@gmail.com>
2020-08-07 23:44:16 +0500

    third and last plz

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static('./dist/jr-app'));
+app.use(express.static(__dirname + '/dist/jr-app'));
 
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/jr-app/'}
-  );
+    //res.sendFile('index.html', {root: 'dist/jr-app/'}
+    res.sendFile(path.join(__dirname + '/dist/jr-app/jrecords'));
+  //);
   });
 
   app.listen(process.env.PORT || 8080);
